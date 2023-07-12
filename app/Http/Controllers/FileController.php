0391e25e80dd8033eb2ed64205a0e17a44c86a2f
mictest23 <mictestone@gmail.com>
2023-07-12 15:41:48 +0800

    first commit to fileupload

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    //
+    public function create(){
+        $file = File::all();
+        return view('create', ['file' => $file]);
+    }
+
+    public function store(Request $request){
+
+        $this->validate($request, [
+
+                'filename' => 'required',
+                'filename.*' => 'mimes:doc,pdf,docx,zip'
+
+        ]);
+        
+        
+        if($request->hasfile('filename'))
+         {
+
+            foreach($request->file('filename') as $file)
+            {
+                $name=$file->getClientOriginalName();
+                $file->move(public_path().'/files/', $name);  
+                $data[] = $name;  
+            }
+         }
+
+         $file= new File();
+         $file->filename=json_encode($data);
+         
+        
+        $file->save();
+
+        return back()->with('success', 'Your files has been successfully added');
+    }
+}
---
+++ resources/views/create.blade.php
@@ -0,0 +1,98 @@
+<html lang="en">
+<head>
+  <title>Laravel Multiple File Upload Example</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+      @if (count($errors) > 0)
+      <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+          @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+      @endif
+
+        @if(session('success'))
+        <div class="alert alert-success">
+          {{ session('success') }}
+        </div> 
+        @endif
+
+    <h3 class="jumbotron">Laravel Multiple File Upload</h3>
+<form method="post" action="{{url('file')}}" enctype="multipart/form-data">
+  {{csrf_field()}}
+
+        <div class="input-group control-group increment" >
+          <input type="file" name="filename[]" class="form-control">
+          <div class="input-group-btn"> 
+            <button class="btn btn-success" type="button"><i class="glyphicon glyphicon-plus"></i>Add</button>
+          </div>
+        </div>
+        <div class="clone hide">
+          <div class="control-group input-group" style="margin-top:10px">
+            <input type="file" name="filename[]" class="form-control">
+            <div class="input-group-btn"> 
+              <button class="btn btn-danger" type="button"><i class="glyphicon glyphicon-remove"></i> Remove</button>
+            </div>
+          </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary" style="margin-top:10px">Submit</button>
+
+  </form>        
+  </div>
+
+
+<script type="text/javascript">
+
+
+    $(document).ready(function() {
+
+      $(".btn-success").click(function(){ 
+          var html = $(".clone").html();
+          $(".increment").after(html);
+      });
+
+      $("body").on("click",".btn-danger",function(){ 
+          $(this).parents(".control-group").remove();
+      });
+
+    });
+</script>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+
+        <h1>{{ dd($file->filename) }}</h1>
+
+        @foreach ($file as $files)
+          <tr>
+              <td>
+                 <a href="">{{ $files->filename }}</a>
+                  {{-- @foreach ($file as $f)
+                        <a href="">test</a>
+                  @endforeach --}}
+
+                {{-- <a href="files/{{ str_replace (array('[', ']', '"'), '' , $files->filename); }}">{{ str_replace (array('[', ']', '"'), '' , $files->filename); }}</a> --}}
+                {{-- <a href="files/{{ str_replace (array('[', ']', '"'), '' , $files->filename); }}">{{ $files->filename}}</a> --}}
+               </td>  
+
+          </tr>
+        @endforeach
+    </tbody>
+  </table>
+
+</body>
+</html>
